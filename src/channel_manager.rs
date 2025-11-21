9f9568a519864a9bbf297a7917ad1ec5667527d2
FlorinBD <FlorinBD@users.noreply.github.com>
2025-11-21 20:54:34 +0200

    Update channel_manager.rs

--- src/channel_manager.rs
+++ src/channel_manager.rs
@@ -829,7 +829,7 @@ pub async fn proxy<A: Endpoint<A> + 'static>(
         // build version response for HU
         //let mut response = VersionResponse::new();
         //let mut payload: Vec<u8> = response.write_to_bytes()?;
-        let mut payload: Vec<u8>::new();
+        let mut payload: Vec<u8>;
         payload.insert(0, ((MESSAGE_VERSION_RESPONSE as u16) >> 8) as u8);
         payload.insert(1, ((MESSAGE_VERSION_RESPONSE as u16) & 0xff) as u8);
         payload.insert(2, 0 as u8);
