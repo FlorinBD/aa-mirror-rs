ce748edc9d29df81bc729a1a513e1dcbab3344c8
FlorinBD <FlorinBD@users.noreply.github.com>
2025-12-14 00:11:18 +0200

    Fix unwrap usage in DHU TCP server listener

--- src/io_uring.rs
+++ src/io_uring.rs
@@ -304,7 +304,7 @@ pub async fn io_loop(
         let mut dhu_listener = None;
         if config.dhu {
             info!("{} 🛰️ DHU TCP server: bind to local address",NAME);
-            dhu_listener = Some(TcpListener::bind(bind_addr).await?.unwrap());
+            dhu_listener = Some(TcpListener::bind(bind_addr).unwrap());
             info!("{} 🛰️ DHU TCP server: listening for `Desktop Head Unit` connection...",NAME);
             if let Ok(s) = tcp_wait_for_connection(&mut dhu_listener.as_mut().unwrap()).await {
                 hu_tcp = Some(s);
