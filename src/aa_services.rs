0b598110d967f0c5dce2109ffe2f1f881ae44e8a
FlorinBD <florin_fieni@yahoo.com>
2025-12-25 23:14:30 +0200

    fix tx_srv

--- src/aa_services.rs
+++ src/aa_services.rs
@@ -130,7 +130,7 @@ impl fmt::Display for ServiceType {
         // fmt::Debug::fmt(self, f)
     }
 }
-pub async fn th_sensor_source(ch_id: i32, tx_srv: Sender<Packet>, mut rx_srv: Receiver<Packet>) -> Result<()> {
+pub async fn th_sensor_source(ch_id: i32, tx_srv: &Sender<Packet>, mut rx_srv: Receiver<Packet>) -> Result<()> {
     info!( "{}: Starting...", get_name());
     loop {
         let pkt = rx_srv.recv().await.ok_or("service reader channel hung up")?;
@@ -191,7 +191,7 @@ pub async fn th_sensor_source(ch_id: i32, tx_srv: Sender<Packet>, mut rx_srv: Re
         format!("<i><bright-black> aa-mirror/{}: </>", dev)
     }
 }
-pub async fn th_media_sink_video(ch_id: i32, tx_srv: Sender<Packet>, mut rx_srv: Receiver<Packet>, vcfg:VideoConfig)-> Result<()>{
+pub async fn th_media_sink_video(ch_id: i32, tx_srv: &Sender<Packet>, mut rx_srv: Receiver<Packet>, vcfg:VideoConfig)-> Result<()>{
     //pre-rendered frames using openh264 lib from a C# app (1 frame out of static 800x480 bmp)
     let wait_screen_config_frame: Vec<u8> = vec![0x00, 0x00, 0x00, 0x01, 0x67, 0x42, 0xC0, 0x1E, 0x8C, 0x8D, 0x40, 0x64, 0x1E, 0x90, 0x0F, 0x08, 0x84, 0x6A, 0x00, 0x00, 0x00, 0x01, 0x68, 0xCE, 0x3C, 0x80];
     let wait_screen_first_frame: Vec<u8> = vec![0x00, 0x00, 0x00, 0x01, 0x65, 0xB8, 0x00, 0x04, 0x00, 0x00, 0x78, 0x8C, 0x50, 0x00, 0x27, 0x1C,
@@ -1116,7 +1116,7 @@ pub async fn th_media_sink_video(ch_id: i32, tx_srv: Sender<Packet>, mut rx_srv:
     }
 
 }
-pub async fn th_media_sink_audio_guidance(ch_id: i32, tx_srv: Sender<Packet>, mut rx_srv: Receiver<Packet>, acfg:AudioConfig)-> Result<()>{
+pub async fn th_media_sink_audio_guidance(ch_id: i32, tx_srv: &Sender<Packet>, mut rx_srv: Receiver<Packet>, acfg:AudioConfig)-> Result<()>{
     info!( "{}: Starting...", get_name());
     let mut audio_stream_started:bool=false;
     loop {
@@ -1221,7 +1221,7 @@ pub async fn th_media_sink_audio_guidance(ch_id: i32, tx_srv: Sender<Packet>, mu
         format!("<i><bright-black> aa-mirror/{}: </>", dev)
     }
 }
-pub async fn th_media_sink_audio_streaming(ch_id: i32, tx_srv: Sender<Packet>, mut rx_srv: Receiver<Packet>, acfg:AudioConfig)-> Result<()>{
+pub async fn th_media_sink_audio_streaming(ch_id: i32, tx_srv: &Sender<Packet>, mut rx_srv: Receiver<Packet>, acfg:AudioConfig)-> Result<()>{
     info!( "{}: Starting...", get_name());
     let mut audio_stream_started:bool=false;
     loop {
@@ -1326,7 +1326,7 @@ pub async fn th_media_sink_audio_streaming(ch_id: i32, tx_srv: Sender<Packet>, m
         format!("<i><bright-black> aa-mirror/{}: </>", dev)
     }
 }
-pub async fn th_media_source(ch_id: i32, tx_srv: Sender<Packet>, mut rx_srv: Receiver<Packet>)-> Result<()>{
+pub async fn th_media_source(ch_id: i32, tx_srv: &Sender<Packet>, mut rx_srv: Receiver<Packet>)-> Result<()>{
     info!( "{}: Starting...", get_name());
     loop {
         let pkt = rx_srv.recv().await.ok_or("service reader channel hung up")?;
@@ -1387,7 +1387,7 @@ pub async fn th_media_source(ch_id: i32, tx_srv: Sender<Packet>, mut rx_srv: Rec
         format!("<i><bright-black> aa-mirror/{}: </>", dev)
     }
 }
-pub async fn th_input_source(ch_id: i32, tx_srv: Sender<Packet>, mut rx_srv: Receiver<Packet>)-> Result<()>{
+pub async fn th_input_source(ch_id: i32, tx_srv: &Sender<Packet>, mut rx_srv: Receiver<Packet>)-> Result<()>{
     info!( "{}: Starting...", get_name());
     loop {
         let pkt = rx_srv.recv().await.ok_or("service reader channel hung up")?;
@@ -1447,7 +1447,7 @@ pub async fn th_input_source(ch_id: i32, tx_srv: Sender<Packet>, mut rx_srv: Rec
         format!("<i><bright-black> aa-mirror/{}: </>", dev)
     }
 }
-pub async fn th_vendor_extension(ch_id: i32, tx_srv: Sender<Packet>, mut rx_srv: Receiver<Packet>)-> Result<()>{
+pub async fn th_vendor_extension(ch_id: i32, tx_srv: &Sender<Packet>, mut rx_srv: Receiver<Packet>)-> Result<()>{
     info!( "{}: Starting...", get_name());
     loop {
         let pkt = rx_srv.recv().await.ok_or("service reader channel hung up")?;
@@ -1508,7 +1508,7 @@ pub async fn th_vendor_extension(ch_id: i32, tx_srv: Sender<Packet>, mut rx_srv:
     }
 }
 
-pub async fn th_bluetooth(ch_id: i32, tx_srv: Sender<Packet>, mut rx_srv: Receiver<Packet>)-> Result<()>{
+pub async fn th_bluetooth(ch_id: i32, tx_srv: &Sender<Packet>, mut rx_srv: Receiver<Packet>)-> Result<()>{
     info!( "{}: Starting...", get_name());
     loop {
         let pkt = rx_srv.recv().await.ok_or("service reader channel hung up")?;
