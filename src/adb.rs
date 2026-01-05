5da1e5bd8c733763e53df4002d2eec05bf4d4cc5
FlorinBD <florin_fieni@yahoo.com>
2026-01-05 13:59:44 +0200

    raw adb shell command debug

--- src/adb.rs
+++ src/adb.rs
@@ -13,3 +13,18 @@ pub fn parse_response_lines(rsp: Vec<u8>) -> Result<Vec<Cow<'static, str>>, Stri
 
     Ok(response)
 }
+
+pub fn parse_response_lines_old(rsp: Vec<u8>) ->Result<Vec<String>, String>
+{
+    let lines=String::from_utf8_lossy(&rsp).to_string();
+    let mut response = vec![];
+    if !lines.is_empty() {
+        lines.lines().into_iter().for_each(|line| {
+            let parts: Vec<&str> = line.split("\t").collect();
+            if !parts.is_empty() {
+                response.push(parts[0].to_string());
+            }
+        })
+    };
+    Ok(response)
+}
