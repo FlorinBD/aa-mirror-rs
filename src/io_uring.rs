798c8eef1cd9913250a2f20d006140f9e90363d8
FlorinBD <florin_fieni@yahoo.com>
2026-01-10 12:50:54 +0200

    using tokio_uring for scrcpy socket reading

--- src/io_uring.rs
+++ src/io_uring.rs
@@ -284,7 +284,6 @@ async fn tsk_scrcpy_video(
     mut stream: TcpStream,
     mut cmd_rx: broadcast::Receiver<Packet>,
     video_tx: broadcast::Sender<Packet>,
-    config: AppConfig,
 ) -> Result<()> {
     info!("Starting video server!");
 
@@ -482,12 +481,12 @@ async fn tsk_adb_scrcpy(
                 info!("SCRCPY connected to all 3 sockets");
                 let hnd_scrcpy_video;
                 let hnd_scrcpy_audio;
+                let srv_tx_cloned = srv_tx.clone();
                 hnd_scrcpy_video = tokio_uring::spawn(async move {
                     if let Err(e) = tsk_scrcpy_video(
                                                     video_stream,
                                                     video_cmd_rx.resubscribe(),
-                                                    srv_tx.clone(),
-                                                    config.clone(),
+                                                    srv_tx_cloned.clone(),
                                                     ).await
                     {
                         error!("SCRCPY video task error: {e}");
@@ -497,7 +496,7 @@ async fn tsk_adb_scrcpy(
                     if let Err(e) = tsk_scrcpy_audio(
                                                     audio_stream,
                                                     audio_cmd_rx.resubscribe(),
-                                                    srv_tx.clone(),
+                                                    srv_tx_cloned.clone(),
                                                     ).await
                     {
                         error!("SCRCPY audio task error: {e}");
