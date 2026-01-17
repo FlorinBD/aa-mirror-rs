fc129e2a20b94295e1ebeefb0aec64b2a080d2da
FlorinBD <florin_fieni@yahoo.com>
2026-01-17 20:48:11 +0200

    i-frame-interval as float

--- src/io_uring.rs
+++ src/io_uring.rs
@@ -395,21 +395,7 @@ async fn tsk_scrcpy_video(
             _ => {}
         }
         //Read encapsulated video frames
-        //let (res, buf_hd) = stream.read(&mut header_buf).await;
         header_buf=read_exact(&stream, header_buf).await?;
-        /*let n = res?;
-        if n == 0 {
-            error!("Video connection closed by server?");
-            return Err(Box::new(io::Error::new(
-                io::ErrorKind::Other,
-                "Video connection closed by server?",
-            )));
-        }
-        if n> 12
-        {
-            error!("Video stream read past header size, wanted 12 but got {}", n);
-        }*/
-
         let pts = u64::from_be_bytes(header_buf[0..8].try_into()?);
         let frame_size=(u32::from_be_bytes(header_buf[8..12].try_into()?))-8;//packet size include also header (pts) witch must be discarded
         let key_frame=(pts & 0x4000_0000_0000_0000u64) >0;
@@ -743,7 +729,7 @@ async fn tsk_adb_scrcpy(
             cmd_shell.push(format!("audio_bit_rate={}", audio_bitrate));
             cmd_shell.push(format!("max_size={}", video_codec_params.res_w));
             cmd_shell.push("video_codec=h264".to_string());
-            cmd_shell.push(format!("video_codec_options=profile:int=1,i-frame-interval:int={}",video_codec_params.fps));//AVC base profile, no B frames, only I and P frames
+            cmd_shell.push(format!("video_codec_options=profile:int=1,i-frame-interval:float={}",video_codec_params.fps));//AVC base profile, no B frames, only I and P frames
             cmd_shell.push(format!("video_bit_rate={}", video_codec_params.bitrate));
             cmd_shell.push(format!("new_display={}x{}/{}", video_codec_params.res_w, video_codec_params.res_h, video_codec_params.dpi));
             cmd_shell.push(format!("max_fps={}", video_codec_params.fps));
