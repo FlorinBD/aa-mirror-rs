a4dec504bac9d81b257b92832c9545c66c5f25cd
FlorinBD <florin_fieni@yahoo.com>
2026-01-18 21:10:54 +0200

    media management changed

--- src/aa_services.rs
+++ src/aa_services.rs
@@ -1416,7 +1416,7 @@ pub async fn th_media_sink_audio_streaming(ch_id: i32, enabled:bool, tx_srv: Sen
                             error!( "{} response send error",get_name());
                         };
                     }
-                    else if cmd == CustomCommand::MD_CONNECTED {
+                    else if cmd == CustomCommand::MD_CONNECTED as i32 {
                         info!("{} MD connected, send media STOP to HU",get_name());
                         md_connected=true;
                         //Send STOP to HU
