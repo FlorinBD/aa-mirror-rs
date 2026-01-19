aa5097a1e5293874172e67f32b7f0ccd1bd3e2c7
FlorinBD <florin_fieni@yahoo.com>
2026-01-19 20:56:14 +0200

    send media stop and wait

--- src/aa_services.rs
+++ src/aa_services.rs
@@ -997,15 +997,16 @@ pub async fn th_media_sink_video(ch_id: i32, enabled:bool, tx_srv: Sender<Packet
                         md_connected=true;
                         //Send STOP to HU
                         let mut media_stop= Stop::new();
-                        let mut payload: Vec<u8>=media_stop.write_to_bytes()?;
-                        payload.insert(0,((MEDIA_MESSAGE_STOP as u16) >> 8) as u8);
-                        payload.insert( 1,((MEDIA_MESSAGE_STOP as u16) & 0xff) as u8);
+                        let mut payload: Vec<u8>=Vec::new();
+                        payload.extend_from_slice(&(ControlMessageType::MEDIA_MESSAGE_STOP as u16).to_be_bytes());
+                        payload.extend_from_slice(&(media_stop.write_to_bytes()));
                         let pkt_rsp = Packet {
                             channel: ch_id as u8,
                             flags: ENCRYPTED | FRAME_TYPE_FIRST | FRAME_TYPE_LAST,
                             final_length: None,
                             payload: payload,
                         };
+
                         if let Err(_) = tx_srv.send(pkt_rsp).await{
                             error!( "{} send error",get_name());
                         };
