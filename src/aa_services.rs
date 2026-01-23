92e7ffcbd5d181191e21df3e9150516e464b1595
FlorinBD <florin_fieni@yahoo.com>
2026-01-23 17:15:22 +0200

    scrcpy control channel improvement

--- src/aa_services.rs
+++ src/aa_services.rs
@@ -1613,10 +1613,12 @@ pub async fn th_input_source(ch_id: i32, enabled:bool, tx_srv: Sender<Packet>, m
                     }
                     else {
                         let binding_req = KeyBindingRequest::new();
-                        binding_req.set_keycodes(keys);
-                        let mut payload: Vec<u8> = Vec::new();
-                        payload.extend_from_slice(&(InputMessageId::INPUT_MESSAGE_KEY_BINDING_REQUEST as u16).to_be_bytes());
-                        payload.extend_from_slice(&binding_req.write_to_bytes().expect("serialization failed"));
+                        binding_req.mut_keycodes().extend_from_slice(&keys);
+
+                        let payload: Vec<u8> = [
+                            &(InputMessageId::INPUT_MESSAGE_KEY_BINDING_REQUEST as u16).to_be_bytes()[..],
+                            &binding_req.write_to_bytes().expect("serialization failed")
+                        ].concat();
                         let pkt_rsp = Packet {
                             channel: ch_id as u8,
                             flags: ENCRYPTED | FRAME_TYPE_FIRST | FRAME_TYPE_LAST,
