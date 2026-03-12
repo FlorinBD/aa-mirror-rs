da4908fe40553e67e5fa08b671ceb280a514fb16
FlorinBD <florin_fieni@yahoo.com>
2026-03-12 09:17:14 +0200

    start video directly

--- src/aa_services.rs
+++ src/aa_services.rs
@@ -1180,25 +1180,8 @@ pub async fn th_media_sink_video(ch_id: i32, enabled:bool, tx_srv: Sender<Packet
                     {
                         if projection_state==ProjectionStatus::FirstScreen
                         {
-
-                            if dhu
-                            {
-                                start_scrcpy_media(&scrcpy_cmd, ch_id as u8, &video_params).await?;
-                                stop_media(&tx_srv, ch_id as u8).await?;
-                                //session_id +=1;
-                                start_media(&tx_srv, ch_id as u8, session_id).await?;
-                                projection_state=ProjectionStatus::ProjectedRecording;
-                            }
-                            else
-                            {
-                                stop_media(&tx_srv, ch_id as u8).await?;
-                                Delay::new(Duration::from_millis(200)).await; // allow HU to process STOP
-                                //session_id +=1;
-                                start_media(&tx_srv, ch_id as u8, session_id).await?;
-                                projection_state=ProjectionStatus::TransitionToProjected;
-                            }
-
-
+                            start_scrcpy_media(&scrcpy_cmd, ch_id as u8, &video_params).await?;
+                            projection_state=ProjectionStatus::ProjectedRecording;
                         }
                         else if projection_state==ProjectionStatus::TransitionToFS
                         {
@@ -1290,21 +1273,15 @@ pub async fn th_media_sink_video(ch_id: i32, enabled:bool, tx_srv: Sender<Packet
                         {
                             if projection_state==ProjectionStatus::TransitionToFS
                             {
-                                start_scrcpy_media(&scrcpy_cmd, ch_id as u8, &video_params).await?;
                                 session_id +=1;
                                 start_media(&tx_srv, ch_id as u8, session_id).await?;
+                                start_scrcpy_media(&scrcpy_cmd, ch_id as u8, &video_params).await?;
                                 projection_state=ProjectionStatus::ProjectedRecording;
                             }
                             else if projection_state==ProjectionStatus::FirstScreen
                             {
-                                if dhu
-                                {
-                                    stop_media(&tx_srv, ch_id as u8).await?;
-                                    start_scrcpy_media(&scrcpy_cmd, ch_id as u8, &video_params).await?;
-                                    session_id +=1;
-                                    start_media(&tx_srv, ch_id as u8, session_id).await?;
-                                    projection_state=ProjectionStatus::ProjectedRecording;
-                                }
+                                start_scrcpy_media(&scrcpy_cmd, ch_id as u8, &video_params).await?;
+                                projection_state=ProjectionStatus::ProjectedRecording;
                             }
                             else if projection_state==ProjectionStatus::ProjectedPause
                             {
