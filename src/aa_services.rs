74fd3743d28ed533d0b7635a3a028549a7e23914
FlorinBD <florin_fieni@yahoo.com>
2026-03-12 15:04:48 +0200

    test video re-config

--- src/aa_services.rs
+++ src/aa_services.rs
@@ -1242,6 +1242,8 @@ pub async fn th_media_sink_video(ch_id: i32, enabled:bool, tx_srv: Sender<Packet
                         video_params.max_unack=rsp.max_unacked();
                         if projection_state==ProjectionStatus::TransitionToProjected
                         {
+                            session_id +=1;
+                            start_media(&tx_srv, ch_id as u8, session_id).await?;
                             start_scrcpy_media(&scrcpy_cmd, ch_id as u8, &video_params).await?;
                             projection_state=ProjectionStatus::ProjectedRecording;
                         }
