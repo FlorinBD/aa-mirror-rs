da054fc71d59f9df49894b927f26dbbc20da03e8
FlorinBD <florin_fieni@yahoo.com>
2026-03-13 14:57:43 +0200

    wait after CONFIG

--- src/aa_services.rs
+++ src/aa_services.rs
@@ -1213,7 +1213,7 @@ pub async fn th_media_sink_video(ch_id: i32, enabled:bool, tx_srv: Sender<Packet
                             info!( "{}, channel {:?}: MD connected, starting video streaming", get_name(), pkt.channel);
                             //tokio::time::sleep(Duration::from_millis(HU_CONFIG_DELAY_MS)).await;
                             Delay::new(Duration::from_millis(HU_CONFIG_DELAY_MS)).await;
-                            //session_id +=1;
+                            session_id +=1;
                             start_media(&tx_srv, ch_id as u8, session_id).await?;
                             start_scrcpy_media(&scrcpy_cmd, ch_id as u8, &video_params).await?;
                             projection_state=ProjectionStatus::FirstScreen;
