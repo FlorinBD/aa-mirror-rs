caf74faddefb6ab2ac2b3de1dd68031ceec28c0a
FlorinBD <florin_fieni@yahoo.com>
2026-08-27 16:03:50 +0300

    code cleanup

--- src/scrcpy.rs
+++ src/scrcpy.rs
@@ -835,7 +835,7 @@ impl ControlServer {
 								Some(pkt) => {
 									// Received a packet
 									let message_id: i32 = u16::from_be_bytes(pkt.payload[0..=1].try_into().unwrap()).into();
-									info!("tsk_scrcpy_control Received command id {:?}", message_id);
+									//info!("tsk_scrcpy_control Received command id {:?}", message_id);
 									if message_id == InputMessageId::INPUT_MESSAGE_INPUT_REPORT  as i32
 									{
 										let data = &pkt.payload[2..]; // start of message data, without message_id
