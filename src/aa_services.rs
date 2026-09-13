b1d6a34a79dc886b730d64618adbbe2a3f617510
FlorinBD <florin_fieni@yahoo.com>
2026-09-13 11:07:33 +0300

    debug VersionRequest for SDR

--- src/aa_services.rs
+++ src/aa_services.rs
@@ -1913,16 +1913,19 @@ impl ServiceManager {
             if message_id == ControlMessageType::MESSAGE_VERSION_REQUEST as i32
             {
                 let data = &pkt.payload[2..]; // start of message data, without message_id
-                if let Ok(msg) = VersionRequest::parse_from_bytes(&data) {
-                    let version = msg.Version();
-                    let major = (version >> 16) as u16;
-                    let minor = (version & 0xFFFF) as u16;
-                    info!( "{:?} HU VersionRequest: Version=0x{:08X}, Major={}, Minor={} received, sending VersionResponse back...",self.srv_type, version, major, minor);
-                }
-                else {
-                    info!( "{:?} HU version request received, sending VersionResponse back...",self.srv_type);
-                }
+                match VersionRequest::parse_from_bytes(&data) {
+                    Ok(msg) => {
+                        let version = msg.Version();
+                        let major = (version >> 16) as u16;
+                        let minor = (version & 0xFFFF) as u16;
+                        info!("{:?} HU VersionRequest: Version=0x{:08X}, Major={}, Minor={} received, sending VersionResponse back...",self.srv_type,version,major,minor);
+                    }
 
+                    Err(e) => {
+                        info!("{:?} HU VersionRequest protobuf parse failed: {:?}, data={:02X?}",self.srv_type,e,data);
+                    }
+                }
+                //info!("{:?} HU version request received, sending VersionResponse back...",self.srv_type);
                 // build version response for HU
                 //let mut response = VersionResponse::new();
                 //let mut payload: Vec<u8> = response.write_to_bytes()?;
